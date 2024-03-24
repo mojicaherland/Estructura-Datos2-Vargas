ed8e63fa5a3a23da2ecbdcd63b38b786e7c45506
Herland Mojica <mojicacardozoh@gmail.com>
2024-03-24 09:34:21 -0400

    lab3

--- src/Lista/ListaPrueba.java
+++ src/Lista/ListaPrueba.java
@@ -223,6 +223,137 @@ public class ListaPrueba {
     //L1.eliminarAlternos() : Método que elimina los elementos
     // de las posiciones alternas. (permanece, se elimina, permanece, se elimina, etc.)
     public void saludar() {
-        System.out.println("Hola");
+        //System.out.println("Hola");
+    }
+
+    //Lab 3- Mas Sobre listas en arreglo --> 21/03/2024
+    //4. L1.insertarLugasAsc(x) : Método que inserta el elemento x,
+    // en su lugar correspondiente en la Lista L1, ordenada de menor a mayor.
+    public void insertarLugarAsc(int x) {
+        for (int i = 0; i < this.cantElement - 1; i++) {
+            if (x > this.arreglo[i] && x < this.arreglo[i + 1]) {
+                this.insertarIesimo(x, i + 1);
+                break;
+            }
+        }
+    }
+
+    //5. L1.insertarLugasDesc(x) : Método que inserta el elemento x,
+    // en su lugar correspondiente en la Lista L1, ordenada de mayor a menor.
+    public void insertarLugarDesc(int x) {
+        for (int i = 0; i < this.cantElement; i++) {
+            if (x < this.arreglo[i] && x > this.arreglo[i + 1]) {
+                this.insertarIesimo(x, i + 1);
+                break;
+            }
+        }
+    }
+
+    //6. L1.comunes(L2, L3) : Método que encuentra en L1, los elementos comunes en las Listas L2, L3.
+    public void comunes(ListaPrueba l2, ListaPrueba l3) {
+        for (int i = 0; i < this.cantElement; i++) {
+            for (int j = 0; j < l2.cantElement; j++) {
+                for (int k = 0; k < l3.cantElement; k++) {
+                    if (this.arreglo[i] == l2.arreglo[j] && this.arreglo[i] == l3.arreglo[k]) {
+                        System.out.println(this.arreglo[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    //7. L1.intercalar(L2, L3) : Método que encuentra en L1, los elementos intercalados de las Listas L2 y L3.
+    public void intercalar(ListaPrueba l2, ListaPrueba l3) {
+        int i = 0;
+        int j = 0;
+        while (i < l2.cantElement && j < l3.cantElement) {
+            this.insertarUltimo(l2.arreglo[i]);
+            this.insertarUltimo(l3.arreglo[j]);
+            i++;
+            j++;
+        }
+        while (i < l2.cantElement) {
+            this.insertarUltimo(l2.arreglo[i]);
+            i++;
+        }
+        while (j < l3.cantElement) {
+            this.insertarUltimo(l3.arreglo[j]);
+            j++;
+        }
+    }
+
+    //8. L1.ordenado() : Método lógico que devuelve True,
+    // si los elementos de la lista L1, están ordenados en forma ascendente o descendente.
+    public boolean ordenado() {
+        boolean asc = true;
+        boolean desc = true;
+        for (int i = 0; i < this.cantElement - 1; i++) {
+            if (this.arreglo[i] > this.arreglo[i + 1]) {
+                asc = false;
+            }
+            if (this.arreglo[i] < this.arreglo[i + 1]) {
+                desc = false;
+            }
+        }
+        return asc || desc;
+    }
+
+    //9. L1.iguales() : Método lógico que devuelve True, si todos los elementos de la lista L1, son iguales.
+    public boolean iguales() {
+        for (int i = 0; i < this.cantElement - 1; i++) {
+            if (this.arreglo[i] != this.arreglo[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //10. L1.diferentes() : Método lógico que devuelve True,
+    // si todos los elementos de la lista L1, son diferentes.
+    public boolean diferentes() {
+        for (int i = 0; i < this.cantElement - 1; i++) {
+            if (this.arreglo[i] != this.arreglo[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //11. L1.menor() : Método que devuelve el menor elemento de la Lista L1.
+    public int menor() {
+        return 0; //falta
+    }
+
+    //12. L1.parImpar() : Método lógico que devuelve True,
+    // en la Lista L1, existe al menos un elemento par y al menor un elemento impar.
+    public boolean parImpar() {
+        boolean par = false;
+        boolean impar = false;
+        for (int i = 0; i < this.cantElement; i++) {
+            if (this.arreglo[i] % 2 == 0) {
+                par = true;
+            } else {
+                impar = true;
+            }
+        }
+        return par && impar;
+    }
+
+    //13. L1.mismaFrec() : Método lógico que devuelve True,
+    // si todos los elementos tienen la misma frecuencia de aparición en la Lista L1.
+    public boolean mismaFrec() {
+        return true;
+    }
+
+    //14. L1.palindrome() : Método lógico que devuelve True,
+    // si los elementos de la Lista L1, forma un palíndrome.
+    public boolean palindrome() {
+        return true;
+    }
+
+    //15. L1.mismosElem(L2) : Método lógico que devuelve True,
+    // las Lista L1 y L2 tienen los mismos elementos.
+    public boolean mismosElem(ListaPrueba l2) {
+        return true;
     }
 }
